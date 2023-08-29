e25d6b6268b198266acb4a42c1a120a263e6af12
unknown <zulfikarmuhamad432@gmail.com>
2023-08-29 09:05:44 +0700

    update

---
+++ app/[agentId]/AgentImage.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+function AgentImage({ agent }: any) {
+    const bgGradient = {
+        background: `
+        url(${agent.background}) no-repeat center,
+        linear-gradient(45deg, #${agent.backgroundGradientColors[0]}, #${agent.backgroundGradientColors[3]})
+    `,
+        backgroundSize: 'cover' // Atur ukuran latar belakang sesuai kebutuhan
+    };
+
+    return (
+        <>
+            <div
+                className="wrapper overflow-hidden border-4 border-gray-900 rounded-lg"
+                style={bgGradient}
+            >
+                <img
+                    src={agent.fullPortraitV2}
+                    alt={agent.displayName}
+                    width={600}
+                    className="object-cover scale-90 animate hover:scale-100 transition-transform duration-5000 "
+                />
+            </div>
+            <audio controls autoPlay className="w-full">
+                <source
+                    src={agent.voiceLine.mediaList[0].wave}
+                    type="audio/wav"
+                />
+                Browsermu tidak mendukung tag audio
+            </audio>
+        </>
+    );
+}
+
+export default AgentImage;
--- app/[agentId]/page.tsx
+++ app/[agentId]/page.tsx
@@ -1,6 +1,6 @@
-import Abilities from '@/app/[agentId]/Abilities';
-import React from 'react';
 import Content from './Content';
+import AgentImage from './AgentImage';
+import Abilities from './Abilities';
 
 const fetchAgent = async (id: string) => {
     const res = await fetch('https://valorant-api.com/v1/agents/' + id);
@@ -10,45 +10,12 @@ const fetchAgent = async (id: string) => {
 async function Detail({ params }: { params: { agentId: string } }) {
     const { data } = await fetchAgent(params.agentId);
 
-    const bgAgent = {
-        background: `
-        url(${data.background}) no-repeat center
-       
-    `,
-        backgroundSize: 'cover' // Atur ukuran latar belakang sesuai kebutuhan
-    };
-
-    const bgGradient = {
-        background: `
-        url(${data.background}) no-repeat center,
-        linear-gradient(45deg, #${data.backgroundGradientColors[0]}, #${data.backgroundGradientColors[3]})
-    `,
-        backgroundSize: 'cover' // Atur ukuran latar belakang sesuai kebutuhan
-    };
-
     return (
         <div className="flex flex-col lg:flex-row items-center gap-10 min-h-[85vh] ">
             <div className="flex-1">
-                <div
-                    className="wrapper overflow-hidden border-2 border-gray-900 rounded-lg"
-                    style={bgGradient}
-                >
-                    <img
-                        src={data.fullPortraitV2}
-                        alt={data.displayName}
-                        width={600}
-                        className="object-cover scale-90 animate hover:scale-100 transition-transform duration-5000 "
-                    />
-                </div>
-                <audio controls autoPlay className="w-full">
-                    <source
-                        src={data.voiceLine.mediaList[0].wave}
-                        type="audio/wav"
-                    />
-                    Browsermu tidak mendukung tag audio
-                </audio>
+                <AgentImage agent={data} />
             </div>
-            <div className="flex-1 border-4 border-gray-800 rounded-lg">
+            <div className="flex-1 border-4 border-gray-800 rounded-lg bg-white">
                 <Content agent={data} />
                 <Abilities abilities={data.abilities} />
             </div>
